a8fa791d6133e35e10426358e44ec1cb38741f0c
Lucas Brito <lucasbrito_9@hotmail.com>
2017-10-22 23:25:42 +0000

    Commit inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+# UFPI-Mobile
---
+++ app/src/main/java/ufpi/br/ufpimobile/TelaHome.java
@@ -0,0 +1,13 @@
+package ufpi.br.ufpimobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_home);
+    }
+}

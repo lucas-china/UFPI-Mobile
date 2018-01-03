8a251eaeb2f26c585b7e61ed5d5f3b7130839a46
Lucas Brito <lucasbrito_9@hotmail.com>
2018-01-03 21:09:01 +0000

    Foi criado a tela de abertura da aplicação.

---
+++ app/src/main/java/ufpi/br/ufpimobile/TelaAbertura.java
@@ -0,0 +1,30 @@
+package ufpi.br.ufpimobile;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaAbertura extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_abertura);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),TelaHome.class);
+                ActivityOptionsCompat activityOptionsCompat= ActivityOptionsCompat.makeCustomAnimation(getApplicationContext(),R.anim.fadein,R.anim.fadeout);
+                ActivityCompat.startActivity(TelaAbertura.this,intent,activityOptionsCompat.toBundle());
+
+                finish();
+
+            }
+        },1000);
+    }
+}

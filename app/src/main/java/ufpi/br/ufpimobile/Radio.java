8f56954394c548e583e6b2b543c982a2b62d8e7a
Lucas Brito <lucasbrito_9@hotmail.com>
2018-01-26 20:22:48 +0000

    Foi atualizado o código da rádio

--- app/src/main/java/ufpi/br/ufpimobile/Radio.java
+++ app/src/main/java/ufpi/br/ufpimobile/Radio.java
@@ -1,5 +1,6 @@
 package ufpi.br.ufpimobile;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
@@ -18,13 +19,11 @@ import java.io.IOException;
 
 public class Radio extends AppCompatActivity {
 
-    private Button botaoPlay;
+    private Button btn;
+    private boolean playPause;
     private MediaPlayer mediaPlayer;
-    private String stream = "http://108.61.20.171:10042/stream/;"; //"http://fm.ufpi.br:8000/admin"
-    private boolean pausado = false;
-    private boolean iniciado = false;
-    private ImageView botao_play_stop;
-    private boolean play = true;
+    private ProgressDialog progressDialog;
+    private boolean initialStage = true;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,82 +44,96 @@ public class Radio extends AppCompatActivity {
         });
 
 
-        botaoPlay = (Button) findViewById(R.id.botaoPlayId);
-        botaoPlay.setEnabled(false);
-        botaoPlay.setText("Carregando...");
-
-
+        btn = (Button) findViewById(R.id.botaoPlayId);
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-        new PlayerTask().execute(stream);
-
-        botaoPlay.setOnClickListener(new View.OnClickListener() {
+        progressDialog = new ProgressDialog(this);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                if(iniciado){
-                    iniciado = false;
-                    mediaPlayer.pause();
-                    botaoPlay.setText("Play");
+            public void onClick(View view) {
+                if (!playPause) {
+                    btn.setText("Pause Streaming");
 
-                }else{
-                    iniciado = true;
-                    mediaPlayer.start();
-                    botaoPlay.setText("Stop");
+                    if (initialStage) {
+                        new Player().execute("http://fm.ufpi.br:8000/admin");
+                    } else {
+                        if (!mediaPlayer.isPlaying())
+                            mediaPlayer.start();
+                    }
 
+                    playPause = true;
+
+                } else {
+                    btn.setText("Play Streaming");
+
+                    if (mediaPlayer.isPlaying()) {
+                        mediaPlayer.pause();
+                    }
+
+                    playPause = false;
                 }
-
             }
         });
-
-
-    }
-
-    private class PlayerTask extends AsyncTask<String, Void, Boolean> {
-
-        @Override
-        protected Boolean doInBackground(String... strings) {
-            try {
-
-                mediaPlayer.setDataSource(strings[0]);
-                mediaPlayer.prepare();
-                pausado=true;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
-            return pausado;
-        }
-
-        @Override
-        protected void onPostExecute(Boolean aBoolean) {
-            super.onPostExecute(aBoolean);
-            botaoPlay.setEnabled(true);
-            botaoPlay.setText("Play");
-        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        if(iniciado){
-            mediaPlayer.pause();
 
+        if (mediaPlayer != null) {
+            mediaPlayer.reset();
+            mediaPlayer.release();
+            mediaPlayer = null;
         }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mediaPlayer.start();
-    }
+    class Player extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            Boolean prepared = false;
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if(pausado){
-            mediaPlayer.release();
+            try {
+                mediaPlayer.setDataSource(strings[0]);
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        initialStage = true;
+                        playPause = false;
+                        btn.setText("PLAY");
+                        mediaPlayer.stop();
+                        mediaPlayer.reset();
+                    }
+                });
+
+                mediaPlayer.prepare();
+                prepared = true;
+
+            } catch (Exception e) {
+                Log.e("MyAudioStreamingApp", e.getMessage());
+                prepared = false;
+            }
+
+            return prepared;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+
+            if (progressDialog.isShowing()) {
+                progressDialog.cancel();
+            }
+
+            mediaPlayer.start();
+            initialStage = false;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog.setMessage("Carregando...");
+            progressDialog.show();
         }
     }
 
